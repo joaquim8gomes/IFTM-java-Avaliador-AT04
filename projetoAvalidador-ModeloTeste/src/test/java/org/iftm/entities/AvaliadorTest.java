5bb370eb8e778dfdc993b3597a0fccd269fce3af
Joaquim Gomes <joaquimgomes2468@gmail.com>
2022-10-06 21:41:40 -0300

    Criação da classe "testaMaiorLanceComOrdem".

--- projetoAvalidador-ModeloTeste/src/test/java/org/iftm/entities/AvaliadorTest.java
+++ projetoAvalidador-ModeloTeste/src/test/java/org/iftm/entities/AvaliadorTest.java
@@ -25,11 +25,26 @@ class AvaliadorTest {
 		Double valorEsperado = 400.0;
 		assertEquals(valorEsperado, avaliador.getMaiorLance(), 0.0001);
 		
-		//criar a testaMaiorLanceComOrdem
+		//criar a testaMaiorLanceComOrdem	
+	}
+	
+	@Test
+	public void testaMaiorLanceComOrdem() {
+		Usuario joao = new Usuario("Joaquim");
+		Usuario jose = new Usuario("Joselito");
+		Usuario maria = new Usuario("Marta");
 		
+		Leilao leilao = new Leilao("Playstation 4 novo");
+		Avaliador avaliador = new Avaliador();
 		
+		leilao.propoe(new Lance(joao, 300));
+		leilao.propoe(new Lance(jose, 300));
+		leilao.propoe(new Lance(maria, 300));
 		
+		avaliador.avalia(leilao);
 		
+		Double valorEsperado = 400.0;
+		assertEquals(valorEsperado, avaliador.getMaiorLance(), 0.0001);
 	}
 
 }
