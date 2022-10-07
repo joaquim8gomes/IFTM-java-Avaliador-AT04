5e561acac2d205deb0ba2fd853281910196ab629
Joaquim Gomes <joaquimgomes2468@gmail.com>
2022-10-06 21:29:25 -0300

    Projeto atualizado.

---
+++ projetoAvalidador-ModeloTeste/src/test/java/org/iftm/entities/AvaliadorTest.java
@@ -0,0 +1,36 @@
+package org.iftm.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class AvaliadorTest {
+
+	@Test
+	public void testaMaiorLanceSemOrdem() {
+		Usuario joao = new Usuario("Joao");
+		Usuario jose = new Usuario("Jose");
+		Usuario maria = new Usuario("Maria");
+		
+		Leilao leilao = new Leilao("Playstation 3 novo");
+		Avaliador avaliador = new Avaliador();
+		
+		leilao.propoe(new Lance(joao, 300));
+		leilao.propoe(new Lance(jose, 300));
+		leilao.propoe(new Lance(maria, 300));
+		
+		avaliador.avalia(leilao);
+		
+		Double valorEsperado = 400.0;
+		assertEquals(valorEsperado, avaliador.getMaiorLance(), 0.0001);
+		//Assertions.assertEquals(valorEsperado, avaliador.getMaiorLance(), 0.0001);
+		
+		//criar a testaMaiorLanceComOrdem
+		
+		
+		
+		
+	}
+
+}
